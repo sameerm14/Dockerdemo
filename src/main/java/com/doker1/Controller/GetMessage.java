c3521c2ffe69c8e964cc6c41000196a6650a36f5
sameerm14 <sameernadaf5761@gmail.com>
2025-05-17 18:23:02 +0530

    First Commit

---
+++ src/main/java/com/doker1/Controller/GetMessage.java
@@ -0,0 +1,13 @@
+package com.doker1.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetMessage {
+
+	@GetMapping("/jenkin")
+	public String message() {
+		return "Hello Jenkins";
+	}
+}
