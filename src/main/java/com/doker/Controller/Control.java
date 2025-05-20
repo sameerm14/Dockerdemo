3ca54fdd5a1353418005c12dd47142e40f156d73
sameerm14 <sameernadaf5761@gmail.com>
2025-05-20 09:42:30 +0530

    Pushed code to git

---
+++ src/main/java/com/doker/Controller/Control.java
@@ -0,0 +1,13 @@
+package com.doker.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Control {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hello Sammy";
+	}
+}
---
+++ src/main/java/com/doker/Docker1Application.java
@@ -0,0 +1,13 @@
+package com.doker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docker1Application.class, args);
+	}
+
+}
